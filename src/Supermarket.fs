e4317b31efbff3db8de048101124a7860f06cb03
Albert Llousas Ortiz <abert.ortizl@gmail.com>
2024-11-21 14:23:55 +0100

    Add case scenario: deal with unknow products

--- src/Supermarket.fs
+++ src/Supermarket.fs
@@ -1,11 +1,20 @@
 module Supermarket
 
+open FSharpx
+
 type Product = { key: string }
+
+type UnknownProduct = UnknownProduct of string
 
 type ShoppingCart = { products: Product list }
 
 let prices = Map.ofList [ ("toothpaste", 0.69); ("toothbrush", 0.99) ]
 
 module Supermarket =
-    
-    let total (cart: ShoppingCart) = cart.products |> List.sumBy (fun product -> prices.[product.key])
+
+  let total (cart: ShoppingCart) : Result<float, UnknownProduct> =
+    let getPrice key = match Map.tryFind key prices with | Some price -> Ok price | None -> Error (UnknownProduct key)
+    cart.products
+    |> List.map (fun product -> getPrice product.key)
+    |> Result.sequence
+    |> Result.map (fun prices -> List.sum prices)
--- tests/Supermarket.Tests.fs
+++ tests/Supermarket.Tests.fs
@@ -10,16 +10,24 @@ let tests = testList "Supermarket tests" [
 
   test "Should calculate the total of an empty shopping cart" {
     let shoppingCart = { products = []}
-    assertThat (Supermarket.total shoppingCart ) 0.0  
+    assertThat (Supermarket.total shoppingCart) (Ok 0.0)  
   }
   
   test "Should calculate the total of a shopping cart with one product" {
     let shoppingCart = { products = [ { key = "toothbrush" } ]}
-    assertThat (Supermarket.total shoppingCart ) 0.99  
+    assertThat (Supermarket.total shoppingCart) (Ok 0.99)  
   }
   
   test "Should calculate the total of a shopping cart with some products" {
     let shoppingCart = { products = [ { key = "toothbrush" }; { key = "toothpaste" } ]}
-    assertThat (Supermarket.total shoppingCart ) 1.68 
+    assertThat (Supermarket.total shoppingCart) (Ok 1.68) 
+  }
+  
+  test "Should fail calculating the total of a shopping cart with an unknown product" {
+    let shoppingCart = { products = [ { key = "unknown-product" } ]}
+    
+    let result = Supermarket.total shoppingCart
+    
+    assertThat result (Error (UnknownProduct "unknown-product"))  
   }
 ]
