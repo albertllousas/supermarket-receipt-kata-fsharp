a9d307a3200588b7fd480cbdea54249bb0e7e0dc
Albert Llousas Ortiz <abert.ortizl@gmail.com>
2024-11-22 09:20:05 +0100

    Refactor: create catalog type and extract find function there

--- src/Supermarket.fs
+++ src/Supermarket.fs
@@ -8,13 +8,17 @@ type UnknownProduct = UnknownProduct of string
 
 type ShoppingCart = { products: Product list }
 
-let prices = Map.ofList [ ("toothpaste", 0.69); ("toothbrush", 0.99) ]
+type Catalog = { prices: Map<string, float> }
+
+module Catalog =
+
+  let getPrice key (catalog: Catalog) =
+    match Map.tryFind key catalog.prices with | Some price -> Ok price | None -> Error(UnknownProduct key)
 
 module Supermarket =
 
-  let total (cart: ShoppingCart) : Result<float, UnknownProduct> =
-    let getPrice key = match Map.tryFind key prices with | Some price -> Ok price | None -> Error (UnknownProduct key)
+  let total (cart: ShoppingCart) (catalog: Catalog): Result<float, UnknownProduct> =
     cart.products
-    |> List.map (fun product -> getPrice product.key)
+    |> List.map (fun product -> Catalog.getPrice product.key catalog)  
     |> Result.sequence
     |> Result.map (fun prices -> List.sum prices)
--- tests/Supermarket.Tests.fs
+++ tests/Supermarket.Tests.fs
@@ -5,28 +5,39 @@ open Supermarket
 
 let assertThat actual expected = Expect.equal expected actual ""
 
+let catalog = { prices = Map.ofList [ ("toothpaste", 0.69); ("toothbrush", 0.99) ] }
+
 [<Tests>]
 let tests = testList "Supermarket tests" [
 
   test "Should calculate the total of an empty shopping cart" {
     let shoppingCart = { products = []}
-    assertThat (Supermarket.total shoppingCart) (Ok 0.0)  
+    
+    let result = Supermarket.total shoppingCart catalog
+    
+    assertThat result (Ok 0.0)  
   }
   
   test "Should calculate the total of a shopping cart with one product" {
     let shoppingCart = { products = [ { key = "toothbrush" } ]}
-    assertThat (Supermarket.total shoppingCart) (Ok 0.99)  
+    
+    let result = Supermarket.total shoppingCart catalog
+    
+    assertThat result (Ok 0.99)  
   }
   
   test "Should calculate the total of a shopping cart with some products" {
     let shoppingCart = { products = [ { key = "toothbrush" }; { key = "toothpaste" } ]}
-    assertThat (Supermarket.total shoppingCart) (Ok 1.68) 
+    
+    let result = Supermarket.total shoppingCart catalog
+    
+    assertThat result (Ok 1.68) 
   }
   
   test "Should fail calculating the total of a shopping cart with an unknown product" {
     let shoppingCart = { products = [ { key = "unknown-product" } ]}
     
-    let result = Supermarket.total shoppingCart
+    let result = Supermarket.total shoppingCart catalog
     
     assertThat result (Error (UnknownProduct "unknown-product"))  
   }
